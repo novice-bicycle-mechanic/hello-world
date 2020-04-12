d67c9551a77ae57cff5b405a1c1a00c9e6dbe8ad
novice-bicycle-mechanic <newmashinostroitel@yandex.ru>
2020-04-12 21:51:58 +0500

    изменения на компьютере

--- ProjectGit/MyNewFile.cpp
+++ ProjectGit/MyNewFile.cpp
@@ -1,1 +1,22 @@
 // MyNewFile.cpp
+
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	//----------------------------------
+
+
+	cout << " Этот файл я создал на GitHab и заполнил на компьютере" << endl;
+
+
+	//----------------------------------
+
+	return 0;
+}
